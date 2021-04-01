592ae89513138938f8e61bf59bdda8e55f1c1608
httpKris <kira7999@gmail.com>
2021-04-01 16:46:50 +0300

    added navbar Companent

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
 // стартовать приложение
 // npm start
 function App() {
   return (
       // чтобы навигация по странице была возможна,  всё приложение нужно обернуть в BrowserRouter
     <BrowserRouter>
+        <NavBar />
      <AppRouter />
     </BrowserRouter>
   );
